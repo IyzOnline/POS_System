3136a5a1e84fa5b2b98e7ff027b4b7ff0a5b2e01
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-11-20 22:24:59 +0800

    feat: Create initial frames for Home Page

---
+++ UI/cashier.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+class App(tk.Tk):
+  def __init__(self):
+    super().__init__()
+    self.title("POS System")
+    self.geometry("960x540")
+    self.initializeExitFS()
+    self.initializeHomePage()
+
+  def initializeExitFS(self):
+    self.attributes('-fullscreen', True)
+    self.bind('<Escape>', self.exit_fullscreen)
+
+  def exit_fullscreen(self, event=None):
+    self.attributes('-fullscreen', False)
+
+  def initializeHomePage(self):
+    self.paddedFrame = tk.Frame(self, bg="#ffffff")
+    self.paddedFrame.pack(expand=True, fill="both", padx=20, pady=20)
+
+    self.sidebarFrame = tk.Frame(self.paddedFrame, bg="#3498db")
+    self.menuFrame = tk.Frame(self.paddedFrame, bg="#49a3df")
+    self.summaryFrame = tk.Frame(self.paddedFrame, bg="#5faee3")
+
+    self.sidebarFrame.place(relx=0, rely=0, relwidth=0.1, relheight=1.0)
+    self.menuFrame.place(relx=0.1, rely=0, relwidth=0.6, relheight=1.0)
+    self.summaryFrame.place(relx=0.72, rely=0, relwidth=0.28, relheight=1.0)
+
+  
+
+app = App()
+app.mainloop()
